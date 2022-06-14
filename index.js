6e83edfe5d6a59103f8744a9184acd96548a54b7
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-14 10:51:34 +0300

    commit tree a59555f561b469628315a518263f562e9d691a68

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) =>
+    tutorial.split(" ")
+        .map((word) => word.slice(0, 1).toUpperCase() + word.slice(1))
+     
+      .join(" ")
+  );
+};  
+
